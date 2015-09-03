0bc1c706dfba4370ab5cf67abc97d989ec0c1ada
Aradhana Singh <aradhana1589@gmail.com>
2015-09-02 23:22:56 -0400

    Update movie.py

--- movie.py
+++ movie.py
@@ -6,10 +6,4 @@ class Movie:
         self.poster_image_url = poster_url
         self.trailer_youtube_url = youtube_link
 
-'''
-#to check if objects of class are being correctly initialzed
-if __name__=="__main__""
-    movie1=Movie("movie1","movie1","movie1")
-    print movie1.title
-    print movie1.poster_image_url
-'''
+
